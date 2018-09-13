0a27c9b2478d1902a77e56e47c03501aa984889a
Piskunou Yahor <egor.piskunou@gmail.com>
2018-09-13 16:19:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let res = 0;
+  while(number > 5) {
+    number = Math.floor(number/5);
+    res+=number
+  }
+  return res;
 }
